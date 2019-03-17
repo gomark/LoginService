5b25309426fa685aa11feb32056141fba34179e8
Putti Tangtrakulwongse <puttitang@puttitang-macbookpro2.roam.corp.google.com>
2019-03-17 22:56:32 +0700

    comments..

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000;
+
+app.listen(port);
+
+console.log('todo list RESTful API server startedd on: ' + port);
+
+app.get("/url", (req, res, next) => {
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+});
