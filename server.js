acbe73fb9d08056dc602b36b6f47e6a896bf01a1
Putti Tangtrakulwongse <puttitang@puttitang-macbookpro2.roam.corp.google.com>
2019-03-20 06:54:52 +0700

    Test log

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ app.listen(port);
 console.log('todo list RESTful API server startedd on: ' + port);
 
 app.get("/loginService", (req, res, next) => {
-  console.log("1")
+  console.log("DEBUG (1)");
+  console.log("INFO (2)");
   const URL_SERVICE1 = process.env.URL_SERVICE1;
   const URL_SERVICE2 = process.env.URL_SERVICE2;
   res.json([URL_SERVICE1, "Lisa-xx", "Michael", URL_SERVICE2, req.query.userId]);
