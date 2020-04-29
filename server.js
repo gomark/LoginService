da86ec8b0a1e39a4b98919ae91dd31d3b4916d7d
Putti Tangtrakulwongse <puttitang@puttitang-macbookpro2.roam.corp.google.com>
2020-04-29 20:20:23 +0700

    add healthcheck

--- server.js
+++ server.js
@@ -6,10 +6,36 @@ app.listen(port);
 
 console.log('todo list RESTful API server startedd on: ' + port);
 
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .send('OK')
+    .end();
+});
+
 app.get("/loginService", (req, res, next) => {
   console.log("DEBUG (1)");
   console.log("INFO (2)");
   const URL_SERVICE1 = process.env.URL_SERVICE1;
   const URL_SERVICE2 = process.env.URL_SERVICE2;
   res.json([URL_SERVICE1, "Putti-Lisa-xxxyy", "Michael", URL_SERVICE2, req.query.userId]);
+});
+
+app.post('/test-post', (req, res) => {
+  var len = 0;
+  req.on('data', chunk => {
+    console.log(`${chunk}`);
+    len += len
+  })
+
+  req.on('end', () => {
+    console.log("== end ==");
+  })  
+
+  /*
+  res
+    .status(200)
+    .send('Received your post, thanks.')
+    .end();
+  */
 });
